0ac717d7a890d14e3d3088cc42cec8f4cf8cd514
Thomas <93293510+ta-torres@users.noreply.github.com>
2025-10-07 22:19:27 -0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>GBC-TS</h1>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
